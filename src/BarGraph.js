a60e6ca3a6d861cd7014c31137f7dbedba40ab0b
blkgoose <alessio.biancone@gmail.com>
2020-05-27 20:33:56 +0200

    feat: Aggiunta visualizzazione della percentuale per la distribuzione delle barre

--- src/BarGraph.js
+++ src/BarGraph.js
@@ -1,18 +1,34 @@
 import React from 'react'
-import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
+import { Area, ComposedChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, LabelList } from 'recharts'
 
-const BarGraph = props =>
-  <BarChart
-    width={props.width}
-    height={props.height}
-    data={props.data}
-    barSize={20}
-  >
-    <XAxis dataKey="name" scale="point" padding={{ left: 10, right: 10 }} />
-    <YAxis />
-    <Tooltip />
-    <CartesianGrid strokeDasharray="3 3" />
-    <Bar dataKey="value" barSize={5} fill="#82ca9d" background={{ fill: '#eee' }} />
-  </BarChart>
+const BarGraph = props => {
+  const totalVal =
+    props.data
+    .map(r => r.value)
+    .reduce((A, B) => A + B, 0)
+
+  const data =
+    props.data.map(r => {
+        r["percentage"] = (r.value / totalVal * 100).toFixed(2)
+
+        return r
+    })
+
+  return (
+    <ComposedChart
+      width={props.width}
+      height={props.height}
+      data={data}
+      barSize={20}
+    >
+      <XAxis dataKey="name" scale="point" padding={{ left: 2, right: 2 }} />
+      <YAxis />
+      <Tooltip />
+      <CartesianGrid />
+      <Bar dataKey="value" barSize={5} fill="#82ca9d" background={{ fill: '#eee' }} />
+      <Area dataKey="percentage" fill="#8884d8" unit="%" />
+    </ComposedChart>
+  )
+}
 
 export default BarGraph
